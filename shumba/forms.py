5feb3d9c28bf65bedbb68b486fb73a0934bdd5af
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-08-20 22:22:52 +0100

    Add bookings model

--- shumba/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Booking
-
-
-# Auto generate form field based on booking model
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-        fields = ["name", "email", "car_type", "package", "date", "time"]
