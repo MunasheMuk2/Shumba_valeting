0f2423a3b5d098d0f23c62fceaae4410a3e1920e
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-08-21 21:32:32 +0100

    Add date and time widgets

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,3 +6,9 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ["name", "email", "car_type", "package", "date", "time"]
+        # Date widget from - https://www.geeksforgeeks.org/python/django-form-field-custom-widgets/
+        # time field - https://studygyaan.com/django/date-picker-in-django#google_vignette
+        widgets = {
+            "date": forms.SelectDateWidget(),
+            "time": forms.TimeInput(attrs={"type": "time"}),  # clock picker
+        }
