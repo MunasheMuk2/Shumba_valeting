4bb5cb9f0616159d717f95999cde8e9ef98ac7b5
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-09-09 20:04:26 +0100

    Add class to login page and add margin in CSS

--- shumba/settings.py
+++ shumba/settings.py
@@ -56,6 +56,12 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
+# ACCOUNT_FORMS = {
+#     "login": "yourapp.forms.CustomLoginForm",
+# }
+
+# ACCOUNT_EMAIL_VERIFICATION = "none"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
