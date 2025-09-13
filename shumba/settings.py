59cf0c439a7cc6b15618353906ff2f21563cb8ca
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-09-13 22:51:30 +0100

    Amend setting allowed hosts to shumba app and also add secret key to heroku

--- shumba/settings.py
+++ shumba/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ["shumba-app-20d8670cc25d.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definitiongit
