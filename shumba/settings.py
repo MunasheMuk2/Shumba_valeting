97324bfdf67355ef75786d1545aaa8235fbb452a
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-11-24 22:38:35 +0000

    Enable WHITENOISE_USE_FINDERS

--- shumba/settings.py
+++ shumba/settings.py
@@ -160,6 +160,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+WHITENOISE_USE_FINDERS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
